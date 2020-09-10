f2bd8e17d673402688f2536d6f2e876324eabdfa
jiangdw <598965300@qq.com>
2020-09-10 10:49:32 +0800

    init rabbitmq writer

---
+++ rabbitmqwriter/src/main/java/com/alibaba/datax/plugin/writer/rabbitmqwriter/Key.java
@@ -0,0 +1,15 @@
+package com.alibaba.datax.plugin.writer.txtfilewriter;
+
+/**
+ * Created by jiangdw on 2020-09-09.
+ */
+public class Key {
+    public static final String HOST = "host";
+    public static final String PORT = "port";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String VHOST = "vhost";
+    public static final String EXCHANGE = "exchange";
+    public static final String ROUTINGKEY = "routingKey";
+    public static final String QUEUE = "queue";
+}
